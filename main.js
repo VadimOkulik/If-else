a46a89f26c7484f0a376873b6fcab9943f918b6e
Vadim Akulik <lodkaby@gmail.com>
2021-03-03 20:07:06 +0300

    request Serge

---
+++ main.js
@@ -0,0 +1,34 @@
+//Task 1
+const userAge = prompt('сколько тебе лет?');
+const userGender = confirm('Если пол М - жми ок, Ж - отмена');
+
+
+if(userAge>60 && userGender === true) {
+    console.log("Вы дедушка")
+} else if(userAge>20 && userGender === true) {
+    console.log("Вы мужчина")
+} else if(userAge>12 && userGender === true) {
+    console.log("Вы юноша")
+} else if(userAge<12 && userGender === true) {
+    console.log("Вы мальчик")
+} else if(userAge>=60 && userGender === false) {
+    console.log("Вы бабушка")
+} else if(userAge>20 && userGender === false) {
+    console.log("Вы женщина")
+} else if(userAge>12 && userGender === false) {
+    console.log("Вы девушка")
+} else if(userAge<12 && userGender === false) {
+    console.log("Вы девочка")
+} else {
+    console.log("Проверьте данные которые вы ввели")
+}
+
+
+
+
+
+
+
+
+
+
